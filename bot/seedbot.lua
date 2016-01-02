dcf65dc03b6d71654e517345d22d07f4aa3d917c
MRAHS <mrahs@emeil.ir>
2016-01-02 12:18:39 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,8 +224,8 @@ function create_config( )
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v1
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[SBSS Version @
+An advance Administration bot based on yagop/telegram-bot by @Mr_Ah_S
 
 https://github.com/MRAHS/SBSS
 
@@ -238,110 +238,110 @@ Our channels
 @SBSS_Team
 ]],
     help_text = [[
-Commands list :
+:لیست دستورات 
 
 !kick [username|id]
-You can also do it by reply
+حذف فرد از گروه حتی با ریپلای
 
 !ban [ username|id]
-You can also do it by reply
+بن کردن فرد از گروه حتی با ریپلای
 
 !unban [id]
-You can also do it by reply
+آن بن کردن فرد حتی با ریپلای
 
 !who
-Members list
+لیست اعضا
 
 !modlist
-Moderators list
+لیست مدیران
 
 !promote [username]
-Promote someone
+مدیر کردن فرد
 
 !demote [username]
-Demote someone
+عزل مقام فرد
 
 !kickme
-Will kick user
+خروج از گروه
 
 !about
-Group description
+توضیحات گروه
 
 !setphoto
-Set and locks group photo
+تنظیم عکس برای گروه و قفل کردن آن
 
 !setname [name]
-Set group name
+تنظیم نام گروه
 
 !rules
-Group rules
+قوانین گروه
 
 !id
-return group id or user id
+آیدی گروه و آیدی فرد در صورت ریپلای کردن
 
 !help
 
 !lock [member|name|bots]
-Locks [member|name|bots] 
+قفل اعضا | نام | ربات ها
 
 !unlock [member|name|photo|bots]
-Unlocks [member|name|photo|bots]
+باز کردن اعضا|نام|عکس|ربات ها
 
-!set rules <text>
-Set <text> as rules
+!set rules <قوانین>
+تنظیم متن به عنوان قوانین
 
-!set about <text>
-Set <text> as about
+!set about <قوانین>
+تنظیم توضیحات گروه
 
 !settings
-Returns group settings
+نمایش تنظیمات گروه
 
 !newlink
-create/revoke your group link
+ساخت / تازه سازی لینک گروه
 
 !link
-returns group link
+دریافت لینک گروه
 
 !owner
-returns group owner id
+آیدی صاحب گروه
 
 !setowner [id]
-Will set id as owner
+تنظیم فرد به عنوان صاحب گروه
 
-!setflood [value]
-Set [value] as flood sensitivity
+!setflood [تعداد]
+محدود کردن تعداد اسپم
 
 !stats
-Simple message statistics
+دریافت آمار در قالب متن
 
-!save [value] <text>
-Save <text> as [value]
+!save [عدد] <text>
+سیو کردن یک متن با یک شماره
 
-!get [value]
-Returns text of [value]
+!get [عدد]
+نمایش متن سیو شده بر اساس شماره
 
 !clean [modlist|rules|about]
-Will clear [modlist|rules|about] and set it to nil
+پاک کردن مدیران | قوانین | توضیحات
 
 !res [username]
-returns user id
+دریافت آیدی کد فرد برای مثال
 "!res @username"
 
 !log
-will return group logs
+دریافت گزارشات گروه
 
 !banlist
-will return group ban list
+لیست افراد بن شده
 
-**U can use both "/" and "!" 
-
-
-*Only owner and mods can add bots in group
+**شما هم میتوانید از ! استفاده کنید و هم میتوانید از / استفاده کنید 
 
 
-*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
+**فقط مدیران میتوانند در گروه ربات اد کنند
 
-*Only owner can use res,setowner,promote,demote and log commands
+
+*استفاده کنند kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about,settings فقط مدیران میتوانند از دستورات 
+
+*استفاده کند res,setowner,promote,demot,log فقط صاحب گروه میتواند از 
 
 ]]
 

a869fc34101911c02780f85b1711df93be3ab4eb
Mr Ah S <mrahs@emeil.ir>
2016-01-06 22:39:28 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -94,7 +94,7 @@ local function run(msg, matches)
   if matches[1]:lower() == 'SBSS' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /sbss ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /SBSS ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
